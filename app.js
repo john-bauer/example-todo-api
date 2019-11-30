56d3ec5f5b800c19593868df9890e105157e377a
John Bauer <johnbauer126@gmail.com>
2019-11-30 13:44:55 -0500

    set up middleware, serve static page at /

--- app.js
+++ app.js
@@ -6,7 +6,31 @@ const methodOverride = require('method-override');
 const cors = require('cors');
 const app = express();
 
+// setup port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
 });
+
+// middleware
+app.set('views', path.join(__dirname, 'views'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(logger('dev'));
+app.use(methodOverride('_method'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// routes
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+  
+
+// catch 404
+app.get('*', function(req, res) {
+    res.status(404).send({message: 'Oops! Not found.'});
+  });
+  
